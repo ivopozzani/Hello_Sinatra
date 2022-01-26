bf1f37768a27de95c7c35948f14aea0c7450b14f
ivopozzani <ivo.pozzani@gmail.com>
2022-01-26 14:20:12 -0300

    feat: header is now a component

--- meuapp.rb
+++ meuapp.rb
@@ -3,13 +3,16 @@
 require 'sinatra'
 
 get '/' do
+  @page = 'homepage'
   erb :homepage
 end
 
 get '/prs' do
+  @page = 'prs'
   erb :prs
 end
 
 get '/academico' do
+  @page = 'academico'
   erb :academico
 end
