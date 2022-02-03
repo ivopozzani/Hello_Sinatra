0e664bd88c37ea532ff11e2d3a3741e0cb0b579a
ivopozzani <ivo.pozzani@gmail.com>
2022-02-03 10:08:53 -0300

    refactor: code refactored to match code review

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -3,12 +3,14 @@
 require_relative '../meuapp'
 
 describe Data do
-  describe 'Fetchs mocked data' do
-    it '#initialize' do
+  describe '#initialize' do
+    it 'checks file path' do
       expect { Data.new(file: 'error.json') }.to raise_error(RuntimeError, 'File not found')
     end
+  end
 
-    it '#parse JSON file' do
+  describe '#parse_JSON' do
+    it 'returns HASH object' do
       data = Data.new(file: './spec/fixtures/test_data.json')
       expect(data.parse_json).to eq({ 'Familia' => %w[pai mãe], 'Educação' => ['Superior Completo'] })
     end
--- spec/meuapp_spec.rb
+++ spec/meuapp_spec.rb
@@ -3,14 +3,13 @@
 require_relative '../meuapp'
 require 'rack/test'
 
-set :environment, :test
-
 describe 'Server Service' do
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
+
   describe 'Generates routes' do
     it 'loads Home page' do
       get '/'
@@ -25,6 +24,13 @@ describe 'Server Service' do
     it 'loads Academico page' do
       get '/academico'
       expect(last_response).to be_ok
+    end
+  end
+
+  describe 'Unknown pages' do
+    it 'returns page not found status' do
+      get '/unknown'
+      expect(last_response.status).to eq(404)
     end
   end
 end
